98a6c692923665c77459ed0d22bc911a7e4b5991
KyleRattet <krattet@gmail.com>
2015-08-01 08:31:19 -0600

    solution to problem 2

--- js/main.js
+++ js/main.js
@@ -11,13 +11,26 @@ $(document).on('ready', function() {
 
 //PROBLEM 1
 // Define a function called getStudentName that takes a single object as an argument and returns the value of the name property/key.
-var name = {name: 'Michael', age: 27 }
+// var name = {name: 'Michael', age: 27 }
 
-function getStudentName (name) {
-   return name.name;
- }
-console.log(getStudentName(name));
+// function getStudentName (name) {
+//    return name.name;
+//  }
+// console.log(getStudentName(name));
 
+//PROBLEM 2
+// Define a function called getTotalLetters that takes an array of strings as an argument and returns the total number of letters in all strings.
+var stringArray = ['javascript', 'is', 'not', 'python'];
+
+function getTotalLetters(stringArray) {
+  var totalLength = 0;
+  for (var i = 0; i < stringArray.length; i++) {
+    totalLength += stringArray[i].length;
+  }
+  return totalLength;
+}
+
+console.log(getTotalLetters(stringArray));
 
 
 });
