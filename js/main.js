5310c639f3b30c5cd55b84ec2b6ee9d365e44941
KyleRattet <krattet@gmail.com>
2015-08-01 08:22:17 -0600

    solution to problem 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 // add scripts
 
 $(document).on('ready', function() {
-  console.log('sanity check!');
+
+//EXAMPLE
+// function test (str) {
+//   return "test" + str
+// }
+// console.log(test("ing"));
+
+
+//PROBLEM 1
+// Define a function called getStudentName that takes a single object as an argument and returns the value of the name property/key.
+var name = {name: 'Michael', age: 27 }
+
+function getStudentName (name) {
+   return name.name;
+ }
+console.log(getStudentName(name));
+
+
+
 });
